f1fa9b3157165deb604421b92cffb996e5329455
GGhuter05 <104902141+GGhuter05@users.noreply.github.com>
2022-06-15 17:32:41 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,20 +15,22 @@ let dude: game.LedSprite = null
 dude = game.createSprite(2, 2)
 let badguy = game.createSprite(0, 1)
 game.setScore(10)
-if (badguy.get(LedSpriteProperty.X) == 4) {
-    badguy.set(LedSpriteProperty.Y, randint(0, 4))
-    badguy.set(LedSpriteProperty.X, 0)
-}
 basic.forever(function () {
     badguy.change(LedSpriteProperty.X, 1)
     basic.pause(500)
 })
 basic.forever(function () {
-    if (badguy.isTouching(dude)) {
+    if (badguy.get(LedSpriteProperty.X) == dude.get(LedSpriteProperty.X) && badguy.get(LedSpriteProperty.Y) == dude.get(LedSpriteProperty.Y)) {
         game.addScore(-1)
         music.playTone(262, music.beat(BeatFraction.Whole))
     }
     if (game.score() == 0) {
         game.gameOver()
+    }
+})
+basic.forever(function () {
+    if (badguy.get(LedSpriteProperty.X) == 4) {
+        badguy.set(LedSpriteProperty.Y, randint(0, 4))
+        badguy.set(LedSpriteProperty.X, 0)
     }
 })
